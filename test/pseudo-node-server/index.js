f45482cb58cb6ffc3dd7659ea53c178bb30bf277
refracta <58779799+refracta@users.noreply.github.com>
2020-08-20 02:18:20 +0900

    Update index.js

--- test/pseudo-node-server/index.js
+++ test/pseudo-node-server/index.js
@@ -1,3 +1,12 @@
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const http = require("http");
+const httpServer = http.createServer(app);
+const io = require("socket.io")(httpServer);
+const process = require('process');
+const port = process.env.PORT || 80;
+
 const {
     spawn
 } = require('child_process');
@@ -6,8 +15,7 @@ const {
     UnixDgramSocket
 } = require('unix-dgram-socket');
 // https://stackoverflow.com/questions/52807556/process-argv-is-undefined-in-node-js
-const process = require('process');
-const mainSocket = new UnixDgramSocket();
+const socket = new UnixDgramSocket();
 
 var DEFAULT_SERVER_PATH = '/tmp/nethack-webtiles-server';
 var DEFAULT_CLIENT_PATH = '/tmp/nethack-webtiles-client';
@@ -19,15 +27,15 @@ if (argv.length > 0) {
 
 const SERVER_PATH = (pid) => `${DEFAULT_SERVER_PATH}-${pid}`;
 const CLIENT_PATH = () => `${DEFAULT_CLIENT_PATH}-default`;
-const SOCKET_MAP = {};
+const CONNECTION_INFO = {};
 
 const PING_TIMEOUT = 10000;
 
-function handleCore(socket, path, obj) {
+function handleCore(path, obj) {
     switch (obj.msg) {
     case 'debug':
-		console.log('DebugMsg:', obj);
-		break;
+        console.log('DebugMsg:', obj);
+        break;
     default:
         console.log('Unknown Request!');
         console.log(`path: ${path}`, obj);
@@ -35,73 +43,98 @@ function handleCore(socket, path, obj) {
     }
 }
 
-mainSocket.on('error', (error) => {
+socket.on('error', (error) => {
     console.log('MainSocketError:\n', error);
 });
 
-mainSocket.on('message', (message, info) => {
+function send(data, path) {
+    if (!socket.send(JSON.stringify(data) + '\0', path)) {
+        console.log(`SocketSendError(${path}): ${JSON.stringify(data)}\n`);
+        var connectionInfo = CONNECTION_INFO[path];
+        clearInterval(connectionInfo.pingTimeoutCheckIntervalId);
+        clearInterval(connectionInfo.sendPingIntervalId);
+        delete CONNECTION_INFO[path];
+    }
+}
+
+socket.on('message', (message, info) => {
     message = message.toString(UnixDgramSocket.payloadEncoding).substring(0, message.indexOf('\0'));
-    let obj = JSON.parse(message);
-    let pid = info.remoteSocket.split('-').pop();
+    let obj;
+    try {
+        obj = JSON.parse(message);
+    } catch (e) {
+        console.log('Error JSON Parsing:', message);
+        return;
+    }
     let path = info.remoteSocket;
-    let socket = SOCKET_MAP[pid];
-	console.log(obj);
-    if (!socket && obj.msg === 'init_socket') {
+    let connectionInfo = CONNECTION_INFO[path];
+
+    if (!connectionInfo && obj.msg === 'init_socket') {
         console.log('InitSocket:', path);
-        let sendSocket = new UnixDgramSocket();
-        let pingTimeoutCheckIntervalId;
-        let sendPingIntervalId;
-        let lastReceivePingTime;
-        sendSocket.on('error', (error) => {
-            console.error('SendSocketError:', `PID: ${pid}\n`, error);
-            sendSocket.close();
-            clearInterval(pingTimeoutCheckIntervalId);
-            clearInterval(sendPingIntervalId);
-			delete SOCKET_MAP[pid];
-        });
-        sendSocket.on('connect', (path) => {
-            console.log(`SendSocketConnect: ${path}`);
-            lastReceivePingTime = new Date().getTime();
-            sendSocket.send(JSON.stringify({
-                    msg: 'init_socket_end',
-                }) + '\0', path);
-            pingTimeoutCheckIntervalId = setInterval(() => {
-                if (new Date().getTime() - lastReceivePingTime >= PING_TIMEOUT) {
-                    console.error('PingTimeoutError:', `PID: ${pid}`);
-                    sendSocket.close();
-                    clearInterval(pingTimeoutCheckIntervalId);
-                    clearInterval(sendPingIntervalId);
-					delete SOCKET_MAP[pid];
-                }
-            }, Math.floor(PING_TIMEOUT / 3));
-            sendPingIntervalId = setInterval(() => {
-                sendSocket.send(JSON.stringify({
-                        msg: 'ping',
-                    }) + '\0', path);
-            }, Math.floor(PING_TIMEOUT / 3));
-        });
-        sendSocket.connect(SERVER_PATH(obj.pid));
-        SOCKET_MAP[pid] = sendSocket;
-    } else if (socket && obj.msg === 'ping') {
+        console.log(`SendSocketConnect: ${path}`);
+        connectionInfo = {};
+        connectionInfo.path = path;
+        connectionInfo.pid = parseInt(path.split('-').pop());
+        connectionInfo.lastReceivePingTime = new Date().getTime();
+        CONNECTION_INFO[path] = connectionInfo;
+        send({
+            msg: 'init_socket_end'
+        }, path);
+        connectionInfo.pingTimeoutCheckIntervalId = setInterval(() => {
+            if (new Date().getTime() - connectionInfo.lastReceivePingTime >= PING_TIMEOUT) {
+                console.error('PingTimeoutError:', `Path: ${path}`);
+                clearInterval(connectionInfo.pingTimeoutCheckIntervalId);
+                clearInterval(connectionInfo.sendPingIntervalId);
+                delete CONNECTION_INFO[path];
+            }
+        }, Math.ceil(PING_TIMEOUT / 3));
+        connectionInfo.sendPingIntervalId = setInterval(() => {
+            send({
+                msg: 'ping'
+            }, path);
+        }, Math.ceil(PING_TIMEOUT / 3));
+    } else if (connectionInfo && obj.msg === 'ping') {
         console.log(`Ping from ${path}`);
-        lastReceivePingTime = new Date().getTime();
-        socket.send(JSON.stringify({
-                msg: 'pong',
-            }) + '\0', path);
-    } else if (socket && obj.msg === 'pong') {
+        connectionInfo.lastReceivePingTime = new Date().getTime();
+        send({
+            msg: 'pong'
+        }, path);
+    } else if (connectionInfo && obj.msg === 'pong') {
         console.log(`Pong from ${path}`);
-    } else if (socket) {
-        handleCore(socket, path, obj);
+    } else if (connectionInfo) {
+        handleCore(path, obj);
     }
 });
 
-mainSocket.on('listening', (path) => {
+socket.on('listening', (path) => {
     console.log(`MainSocketListening: ${path}`);
 });
 
-mainSocket.bind(CLIENT_PATH());
+socket.bind(CLIENT_PATH());
 
-//for (var i = 0; i < 20; i++) {
-//    var runProcess = spawn(DEFAULT_NETHACK_PATH);
-//    console.log('Run Process! PID:', runProcess.pid);
-//}
+/*
+for (var i = 0; i < 20; i++) {
+var runProcess = spawn(DEFAULT_NETHACK_PATH);
+console.log('Run Process! PID:', runProcess.pid);
+}
+ */
+
+io.on('connection', (socket) => {
+    // console.log(socket);
+    socket.on('connection', (data) => {
+        if (data.msg == 'key') {
+            console.log(data);
+            Object.keys(CONNECTION_INFO).forEach(path => {
+                send(data, path);
+            });
+        }
+    });
+});
+
+app.get("/*", (req, res, next) => {
+    express.static('web')(req, res, next);
+});
+
+httpServer.listen(port, function () {
+    console.log("Listening on port *:" + port);
+});
