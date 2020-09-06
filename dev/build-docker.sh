72fca7506014989eb0bc79ebf59e95ff223c1ed5
refracta <58779799+refracta@users.noreply.github.com>
2020-09-06 10:09:11 +0900

    add build files

---
+++ dev/build-docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git clone https://github.com/NetHack/NetHack -b NetHack-3.6 ./sources/nh360
+cp ./webtiles/nh366/* ./sources/nh366 -r
+
+xhost +local:docker
+sudo docker build -t emalron/nethack ./dev
+sudo docker stop nethack-webtiles-dev &> /dev/null
+sudo docker rm nethack-webtiles-dev &> /dev/null
+sudo docker run -it \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=unix$DISPLAY \
+    -v "$PWD":/usr/src/NetHack-Webtiles \
+	-p 80:80 \
+	-p 81:81 \
+    --name nethack-webtiles-dev \
+    emalron/nethack \
+    bash
