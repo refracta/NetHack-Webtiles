e2dcda775d515b68a924b7ce18fb9bd84fb826d2
refracta <58779799+refracta@users.noreply.github.com>
2021-01-01 19:45:11 +0900

    remove sudo from nethack build script

--- dev/env-debian.sh
+++ dev/env-debian.sh
@@ -15,7 +15,7 @@ git clone https://github.com/NetHack/NetHack -b NetHack-3.6 ./sources/nh366
 cp ./webtiles/nh366/* ./sources/nh366 -r
 cd ./sources/nh366
 sed -i "s#/usr/src/NetHack-Webtiles#$WEBTILES_PATH#g" sys/unix/hints/linux-webtiles
-sudo sh build.sh
+sh build.sh
 cd ../../
 printf "\n\nexport LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib\n\n" >> ~/.bashrc
 sh dev/install-node-dependency.sh
