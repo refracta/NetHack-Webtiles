a9099e8b71eb4cf6996feab83aaf8898c91f803a
Eddy Sung <viviet@tutanota.com>
2021-02-09 21:20:34 +0900

    Add large text popup (#14)
    * added popup
    
    * seperated html/js/css

--- web/js/game-ui-handler.js
+++ web/js/game-ui-handler.js
@@ -577,6 +577,16 @@ class GameUIHandler {
         //old_status = [status1, status2];
     }
 
+    launchLargeTextPopup(text) {
+      document.getElementById('popup-content').innerHTML = text;
+      document.getElementById('ui-popup').style.display = "block";
+    }
+
+    closePopup(){
+      const popup = document.getElementById('ui-popup');
+      document.getElementById('ui-popup').style.display = "none";
+    }
+
 
 }
 
