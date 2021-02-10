18cfb62d62025d55e9108b14ec84d0f0a4f8c632
refracta <58779799+refracta@users.noreply.github.com>
2021-02-10 18:07:05 +0900

    fix selenium module install path

--- dev/setting-fast-launch.ps1
+++ dev/setting-fast-launch.ps1
@@ -1,4 +1,4 @@
-npm i --save selenium-webdriver
 Invoke-WebRequest "https://chromedriver.storage.googleapis.com/88.0.4324.96/chromedriver_win32.zip" -OutFile "chromedriver_win32.zip"
 Expand-Archive chromedriver_win32.zip -DestinationPath .
 Remove-Item chromedriver_win32.zip
+npm install
--- dev/setting-fast-launch.sh
+++ dev/setting-fast-launch.sh
@@ -3,4 +3,4 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
 wget https://chromedriver.storage.googleapis.com/88.0.4324.96/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
-npm i --save selenium-webdriver
+npm install
