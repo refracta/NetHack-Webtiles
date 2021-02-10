c4eaeaa322e6fb93b2fc258319f02e3e6bab23ca
refracta <58779799+refracta@users.noreply.github.com>
2021-02-10 17:31:34 +0900

    Implement automatic test (#15)
    * Refactoring build script
    
    * add dev scripts
    
    * add unix-dgram-socket & webdriver path
    
    * add validation script
    
    * add default CMake file
    
    * fix unix-dgram-socket path

---
+++ dev/copy2webtiles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp sources/nh366/win/webtiles/webtiles.c webtiles/nh366/win/webtiles/webtiles.c
+cp sources/nh366/win/tty/wintty.c webtiles/nh366/win/tty/wintty.c
+cp sources/nh366/win/tty/wintty.c webtiles/nh366/win/tty/wintty.c
+cp sources/nh366/include/webtiles.h webtiles/nh366/include/webtiles.h
+cp sources/nh366/sys/unix/unixmain.c webtiles/nh366/sys/unix/unixmain.c
