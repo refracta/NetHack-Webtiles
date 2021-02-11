ad3fe9e3aede49484d02639ce0a6681e8ed616ac
refracta <58779799+refracta@users.noreply.github.com>
2021-02-12 07:17:28 +0900

    add getline.c, topl.c, termcap.c

--- dev/copy2webtiles.sh
+++ dev/copy2webtiles.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cp sources/nh366/win/webtiles/webtiles.c webtiles/nh366/win/webtiles/webtiles.c
 cp sources/nh366/win/tty/wintty.c webtiles/nh366/win/tty/wintty.c
-cp sources/nh366/win/tty/wintty.c webtiles/nh366/win/tty/wintty.c
+cp sources/nh366/win/tty/getline.c webtiles/nh366/win/tty/getline.c
+cp sources/nh366/win/tty/topl.c webtiles/nh366/win/tty/topl.c
+cp sources/nh366/win/tty/termcap.c webtiles/nh366/win/tty/termcap.c
 cp sources/nh366/include/webtiles.h webtiles/nh366/include/webtiles.h
 cp sources/nh366/sys/unix/unixmain.c webtiles/nh366/sys/unix/unixmain.c
