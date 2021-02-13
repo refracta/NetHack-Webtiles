4b63a705bc2a68578d369726c0938cb8fd219171
refracta <58779799+refracta@users.noreply.github.com>
2021-02-13 11:29:55 +0900

    Fix nethack config (#19)
    * add path replace command
    
    * set MAXPLAYERS=0, CHECK_SAVE_UID=0

--- dev/install-nethack-source.sh
+++ dev/install-nethack-source.sh
@@ -5,5 +5,6 @@ git clone https://github.com/NetHack/NetHack -b NetHack-3.6 ./sources/nh366
 cp ./webtiles/nh366/* ./sources/nh366 -r
 cd ./sources/nh366
 sed -i "s#/usr/src/NetHack-Webtiles#$WEBTILES_PATH#g" sys/unix/hints/linux-webtiles
+sed -i "s#/usr/src/NetHack-Webtiles#$WEBTILES_PATH#g" sys/unix/sysconf
 cd ../../
 sed -i "s#/usr/src/NetHack-Webtiles#$WEBTILES_PATH#g" games.json
