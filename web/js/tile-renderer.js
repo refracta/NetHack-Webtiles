e03b960ba643d0147350ba029f03b96e990fb52d
refracta <58779799+refracta@users.noreply.github.com>
2021-03-10 11:43:55 +0900

    fix dragCount check condition

--- web/js/tile-renderer.js
+++ web/js/tile-renderer.js
@@ -316,7 +316,7 @@ class TileRenderer {
         let [x, y] = to2DXY(i);
         this.cursorX = x;
         this.cursorY = y;
-        if(this.dragCount === 0){
+        if(this.dragCount != 0){
             this.camera.centerOn(this.tileConfig.tileWidth * this.cursorX + this.tileConfig.tileWidth / 2,
                 this.tileConfig.tileHeight * this.cursorY + this.tileConfig.tileHeight / 2);
         }
